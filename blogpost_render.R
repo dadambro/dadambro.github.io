d86f87ae08637e1e6fe0d8a5a81fa75d5c76a7c0
Damon <dadambro@ncsu.edu>
2023-07-27 17:31:23 -0400

    Creating fifth blog post.

--- blogpost_render.R
+++ blogpost_render.R
@@ -1,1 +1,1 @@
-render("./_Rmd/2023-07-06-Project-2.Rmd", output_format = "github_document", output_dir = "./_posts", output_options = list(html_preview = FALSE))
+render("./_Rmd/2023-07-19-blog-post-4.Rmd", output_format = "github_document", output_dir = "./_posts", output_options = list(html_preview = FALSE))
